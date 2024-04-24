41676c061b94d3a61bc3a33d4c774f0f7dfd52eb
Gunner Robinson <gunnerwrobinson@gmail.com>
2024-04-25 01:43:18 +0200

    working history retrieval on popup

--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,15 @@
-console.log("This is a popup!")
+function fetchHistory() {
+    // Adjust the 'text' and 'maxResults' to fetch more history items
+    chrome.history.search({text: '', maxResults: 10}, function(data) {
+        var historyList = document.getElementById('historyList');
+        historyList.innerHTML = '';
+
+        data.forEach(function(page) {
+            var li = document.createElement('li');
+            li.textContent = `${page.url}`;
+            historyList.appendChild(li);
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', fetchHistory);
