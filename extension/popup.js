0d8d9c610eb946b294704f319b677f6d7968e650
Gunner Robinson <gunnerwrobinson@gmail.com>
2024-05-02 00:55:12 +0200

    auth0 trying to set up login with server

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 function fetchHistory() {
     // Adjust the 'text' and 'maxResults' to fetch more history items
     chrome.history.search({text: '', maxResults: 10}, function(data) {
@@ -12,4 +14,32 @@ function fetchHistory() {
     });
 }
 
+// Allow account creation TODO: make this more secure
+document.getElementById('accountForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // send to database via http request
+    fetch('http://localhost:3000/create-account', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({username: username, password: password})
+    })
+    .then(response => response.text())
+    .then(data => alert('Account created successfully!'))
+    .catch(error => this.console.error('Error: ', error));
+
+    /*
+    //stored locally
+    chrome.storage.local.set({username: username, password: password}, function() {
+        console.log('Account created for', username);
+        alert('Account created successfully');
+    });
+    */
+});
+
+// Get history on popup load
 document.addEventListener('DOMContentLoaded', fetchHistory);
