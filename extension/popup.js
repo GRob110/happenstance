b2bae6fce846160b230ace6aeef9ae0fa939740c
Gunner Robinson <gunnerwrobinson@gmail.com>
2024-05-06 14:47:35 +0200

    happenstance code working up to page redirect which fails

--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,29 @@
-setTimeout(() => {
-    document.addEventListener('DOMContentLoaded', () => {
-        const loginButton = document.getElementById('loginBtn');
-        console.log('Login button found: ', !!loginButton);
-        if (loginButton) {
-            loginButton.addEventListener('click', () => {
-                chrome.runtime.getBackgroundPage((backgroundPage) => {
-                    backgroundPage.beginAuth();
-                });
-            });
-        } else {
-            console.error('No login button found');
+const authUrl = `https://dev-6q2l8zmczwn7un71.us.auth0.com/authorize` +
+    `?client_id=NRfoOLMeJcJLxqS5i2carZc9EmP3ODaV` +
+    `&response_type=code` +
+    `&redirect_uri=${encodeURIComponent(chrome.identity.getRedirectURL())}` +
+    `&scope=openid profile email` +
+    `&code_challenge_method=S256`;
+
+function beginAuth() {
+    console.log('Beginning auth flow');
+
+    chrome.identity.launchWebAuthFlow({
+        url: authUrl,
+        interactive: true
+    }, function(redirectUrl) {
+        if (chrome.runtime.lastError || !redirectUrl) {
+            alert(chrome.runtime.lastError ? chrome.runtime.lastError.message : 'No redirect URL');
+            return;
         }
+        alert('Authorization redirect URL:', redirectUrl);
     });
-}, 1000);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginButton = document.getElementById('loginBtn');
+    loginButton.addEventListener('click', () => {
+        console.log("login button clicked");
+        beginAuth();
+    });
+});
---
+++ test/popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const helloButton = document.getElementById('helloBtn');
+    helloButton.addEventListener('click', () => {
+        console.log("hello world");
+    });
+});
