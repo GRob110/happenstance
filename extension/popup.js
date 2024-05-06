d0696b3d0702b572ed5e611bc38dc210c1ca05f6
Gunner Robinson <gunnerwrobinson@gmail.com>
2024-05-06 16:02:08 +0200

    extension link working!

--- extension/popup.js
+++ extension/popup.js
@@ -1,22 +1,34 @@
-const authUrl = `https://dev-6q2l8zmczwn7un71.us.auth0.com/authorize` +
-    `?client_id=NRfoOLMeJcJLxqS5i2carZc9EmP3ODaV` +
-    `&response_type=code` +
-    `&redirect_uri=${encodeURIComponent(chrome.identity.getRedirectURL())}` +
-    `&scope=openid profile email` +
-    `&code_challenge_method=S256`;
+const baseAuthUrl = "https://dev-6q2l8zmczwn7un71.us.auth0.com/authorize";
+const queryParams = new URLSearchParams({
+    client_id: "NRfoOLMeJcJLxqS5i2carZc9EmP3ODaV",
+    response_type: "code",
+    redirect_uri: chrome.identity.getRedirectURL(),
+    scope: "openid profile email",
+    code_challenge_method: "S256"
+});
+const authUrl = `${baseAuthUrl}?${queryParams.toString()}`;
+
 
 function beginAuth() {
     console.log('Beginning auth flow');
+    console.log('authUrl: ', authUrl);
+    console.log("Generated Redirect URL: ", chrome.identity.getRedirectURL());
+
 
     chrome.identity.launchWebAuthFlow({
         url: authUrl,
         interactive: true
     }, function(redirectUrl) {
-        if (chrome.runtime.lastError || !redirectUrl) {
-            alert(chrome.runtime.lastError ? chrome.runtime.lastError.message : 'No redirect URL');
+        if (chrome.runtime.lastError) {
+            console.error("Error during authentication:", chrome.runtime.lastError.message);
             return;
         }
-        alert('Authorization redirect URL:', redirectUrl);
+        if (!redirectUrl) {
+            console.error('No redirect URL');
+            return;
+        }
+        console.log('Authorization redirect URL:', redirectUrl);
+        alert('Authorization redirect URL: ' + redirectUrl);
     });
 }
 
