a345269a39b9d9f6cfebb02bd503a6bce7a1dae3
Gunner Robinson <gunnerwrobinson@gmail.com>
2024-05-14 18:20:02 +0200

    sandbox working, pairing down frontend to work together

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,24 +1,37 @@
 const express = require('express');
-const { auth, requiresAuth } = require('express-openid-connect');
-const { auth0 } = require('../backend/config/keys');
+//const mongoose = require('mongoose');
+//const sessionMiddleware = require('./middleware/sessionMiddleware');
+//const { auth, requiresAuth } = require('express-openid-connect');
+const config = require('./config/keys');
+const authMiddleware = require('./middleware/authMiddleware');
 
 const app = express();
 
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: 'cc5936c2bb14a98ee609d3eb6adbc039fe07f6cc309cd9c81fe8c6ce33a7878a',
-    issuerBaseURL: 'https://dev-6q2l8zmczwn7un71.us.auth0.com',
-    clientID: 'NRfoOLMeJcJLxqS5i2carZc9EmP3ODaV',
-    clientSecret: '25Sc8kRlXVgKLq6HdQYOs4cZ8ZwXg8x_5NQsl_eYmS8zvS70oH_f7We4u-EosspB',
-    baseURL: 'http://localhost:5000',
-};
+//mongoose.connect(config.mongoURI);
 
-app.use(auth(config));
+//app.use(sessionMiddleware);
 
-app.get('/', requiresAuth(), (req, res) => {
-    res.send(`Hello ${req.oidc.user.name}, you are logged in!`);
+app.use(authMiddleware);
+
+app.get('/', /*requiresAuth(),*/ (req, res) => {
+    if (req.oidc.isAuthenticated()) {
+        res.send(`Hello ${req.oidc.user.name}, you are logged in! <a href="/logout">Logout</a>`);
+    } else {
+        res.send('Hello, please <a href="/login">login</a>');
+    }
 });
+
+/*
+app.get('/logout', (req, res) => {
+    req.oidc.logout();
+});
+*/
+
+/*
+app.get('/login', (req, res) => {
+    res.oidc.login();
+});
+*/
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
