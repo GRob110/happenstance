16ce969fb21693d63d1aa19aff8e94371ed7fec5
Gunner Robinson <gunnerwrobinson@gmail.com>
2024-05-15 17:36:47 +0200

    the tutorial code works!!! react + express + ts it is

---
+++ extension/background.js
@@ -0,0 +1,87 @@
+const loginUrl = 'http://localhost:5000/';
+let history = [];
+
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Extension Installed');
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'login') {
+        console.log('login clicked');
+        chrome.tabs.create({ url: loginUrl });
+    } else if (message.type === 'logout') {
+        console.log('logout clicked');
+        chrome.tabs.create({ url: loginUrl });
+    } else if (message.type === 'sync') {
+        console.log('sync clicked');
+        sendHistoryToWebApp();
+    } else if (message.type === 'fetchUser') {
+        console.log('fetchUser');
+        fetch('http://localhost:5000/profile', {
+            credentials: 'include',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                sendResponse({ user: data });
+                return true;
+            })
+            .catch(error => {
+                console.error('Error fetching user', error);
+                sendResponse({ error: error.message });
+                return false;
+            });
+        return true;
+    }
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url) {
+        // get the current timestamp
+        const timestamp = new Date().toISOString();
+        // Sace the URL and timestamp locally
+        history.push(changeInfo.url);
+        chrome.storage.local.set({ history: history });
+        console.log('Set history', history);
+        
+        if (history.length > 10) {
+            // remove entries older than 10
+            history.shift();
+        }
+    }
+});
+
+function sendHistoryToWebApp() {
+    chrome.storage.local.get({ history: [] }, (result) => {
+        history = result.history || [];
+        console.log('Sending history', history);
+        console.log('Sending history', JSON.stringify({history}));
+        //TODO connect to a user
+        fetch('http://localhost:5000/api/history', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({history}),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('History sent', data);
+            })
+            .catch(error => {
+                console.error('Error sending history', error);
+            });
+    });
+}
+
+// send history to web app every 5 minutes
+setInterval(sendHistoryToWebApp, 5 * 60 * 1000);
---
+++ extension/popup.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginButton = document.getElementById('loginBtn');
+    const logoutButton = document.getElementById('logoutBtn');
+    const userInfo = document.getElementById('user-info');
+    const historyList = document.getElementById('history-list');
+    const historySyncButton = document.getElementById('history-sync');
+
+    if (loginButton) {
+        loginButton.addEventListener('click', () => {
+            chrome.runtime.sendMessage({ type: 'login' });
+            console.log('login clicked');
+        });
+    }
+
+    if (logoutButton) {
+        logoutButton.addEventListener('click', () => {
+            chrome.runtime.sendMessage({ type: 'logout' });
+            console.log('logout clicked');
+        });
+    }
+
+    if (historySyncButton) {
+        historySyncButton.addEventListener('click', () => {
+            chrome.runtime.sendMessage({ type: 'sync' });
+            console.log('sync clicked');
+        });
+    }
+
+    chrome.runtime.sendMessage({ type: 'fetchUser' }, (response) => {
+        if (response.user.isAuthenticated) {
+            console.log('User found', response.user.name);
+            userInfo.textContent = `Logged in as ${response.user.name}`;
+            loginButton.style.display = 'none';
+            logoutButton.style.display = 'block';
+        } else if (!response.user.isAuthenticated){
+            console.log('Not logged in');
+            userInfo.textContent = 'Not logged in';
+            loginButton.style.display = 'block';
+            logoutButton.style.display = 'none';
+        } else if (response.error) {
+            console.error('Error fetching user', response.error);
+            userInfo.textContent = `Error: ${response.error}`;
+            loginButton.style.display = 'block';
+            logoutButton.style.display = 'none';
+        } else {
+            console.error('something else');
+        }
+    });
+
+    chrome.storage.local.get({ history: [] }, (result) => {
+        console.log('loading history', result.history);
+        const history = result.history;
+        history.forEach((entry) => {
+            const li = document.createElement('li');
+            li.textContent = `${entry}`;
+            historyList.appendChild(li);
+        });
+    });
+});
