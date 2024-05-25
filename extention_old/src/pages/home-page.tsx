0b2bcd45fd72c69bb11f696293f96b9e2797a8f5
Gunner Robinson <gunnerwrobinson@gmail.com>
2024-05-24 19:32:05 -0700

    chrome extension fully works, just need to make extention_old the only one

--- extention_old/src/pages/home-page.tsx
+++ extention_old/src/pages/home-page.tsx
@@ -1,35 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import { getProtectedResource } from '../services/message.service';
-import { PageLayout } from '../components/page-layout';
+//import { useAuth0 } from '@auth0/auth0-react';
+//import { getProtectedResource } from '../services/message.service';
 
 window.console.log('home-page.tsx');
 
 export const HomePage: React.FC = () => {
-  const [message, setMessage] = useState<string>('');
+  //const [message, setMessage] = useState<string>('');
   const [messageStored, setMessageStored] = useState<string>('');
   const [userInfo, setUserInfo] = useState<string>('');
+  const [authToken, setAuthToken] = useState<string>('');
 
   const domain = import.meta.env.VITE_APP_AUTH0_DOMAIN;
   const server_url = import.meta.env.VITE_APP_API_SERVER_URL;
   const login_url = import.meta.env.VITE_APP_LOGIN_URL;
 
-
-  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
-
-  console.log('isAuthenticated', isAuthenticated);
-
-  let authToken = '';
-
-  chrome.storage.local.get(['authToken'], (result) => {
-    console.log('authToken', result.authToken);
-    if (result.authToken) {
-      authToken = result.authToken;
-      console.log('authToken gotten', result.authToken);
-    } else {
-      console.log('authToken not gotten');
-    }
-  });
+  useEffect(() => {
+    chrome.storage.local.get(['authToken'], (result) => {
+      console.log('authToken', result.authToken);
+      if (result.authToken) {
+        setAuthToken(result.authToken);
+        console.log('authToken gotten', result.authToken);
+      } else {
+        console.log('authToken not gotten');
+      }
+    });
+  }, []);
 
   function login() {
     chrome.tabs.create({ url: login_url });
@@ -39,14 +34,25 @@ export const HomePage: React.FC = () => {
   function logout() {
     chrome.tabs.create({ url: login_url });
     chrome.storage.local.remove(['authToken']);
+    setAuthToken('');
     console.log('logout clicked');
   };
 
   useEffect(() => {
+    if (authToken === '') {
+      return;
+    }
+
     let isMounted = true;
+
+    if (authToken === '') {
+      return;
+    }
 
     // Gets message using the stored auth token
     const getMessageStored = async (authToken: string) => {
+      if (!isMounted) return;
+
       console.log('fetching message');
       try {
         const response = await fetch(`${server_url}/api/messages/protected`, {
@@ -70,33 +76,13 @@ export const HomePage: React.FC = () => {
       }
     }
 
-    // gets api message using token pulled from auth0 directly
-    const getMessage = async () => {
-      const accessToken = await getAccessTokenSilently();
-
-      console.log('access token: ', accessToken);
-      const { data, error } = await getProtectedResource(accessToken);
-
-      if (!isMounted) {
-        return;
-      }
-
-      if (data) {
-        setMessage(JSON.stringify(data, null, 2));
-        console.log("data: ", data);
-      }
-
-      if (error) {
-        setMessage(JSON.stringify(error, null, 2));
-        console.log("error: ",  error);
-      }
-    };
-
     // pulls user info from auth0 with stored token
     const getUserInfo = async (authToken: string) => {
+      if (!isMounted) return;
+
       console.log('fetching message');
       try {
-        const response = await fetch(`https://${domain}/api/messages/protected`, {
+        const response = await fetch(`https://${domain}/userinfo`, {
           headers: {
             'Authorization': `Bearer ${authToken}`,
             'Content-Type': 'application/json'
@@ -118,41 +104,36 @@ export const HomePage: React.FC = () => {
     }
 
     getMessageStored(authToken);
-    getMessage();
     getUserInfo(authToken);
 
 
     return () => {
       isMounted = false;
     };
-  }, [getAccessTokenSilently]);
+  }, [authToken]);
 
-  window.console.log('Home.tsx rendered');
-
-  window.console.log("message :", message);
-  window.console.log("messageStored :", messageStored);
-  window.console.log("userInfo :", userInfo);
+  console.log('Home.tsx rendered');
+  console.log("messageStored :", messageStored);
+  console.log("userInfo :", userInfo);
+  console.log("authToken :", authToken);
 
   return (
-    <PageLayout>
-      <div className="content-layout">
-        <h1 id="page-title" className="content__title">
-          Happenstance
-        </h1>
-        {isAuthenticated && (
-          <div id="content">
-            <p>Protected Message: {message}</p>
-            <p>Protected Message Stored: {messageStored}</p>
-            <p>Protected User Info: {userInfo}</p>
-            <button onClick={logout}>Logout</button>
-          </div>
-        )}
-        {!isAuthenticated && (
-          <div id="content">
-            <button onClick={login}>Login</button>
-          </div>
-        )}
-      </div>
-    </PageLayout>
+    <div className="content-layout">
+      <h1 id="page-title" className="content__title">
+        Happenstance
+      </h1>
+      {authToken !== '' && (
+        <div id="content">
+          <p>Protected Message Stored: {messageStored}</p>
+          <p>Protected User Info: {userInfo}</p>
+          <button onClick={logout}>Logout</button>
+        </div>
+      )}
+      {authToken === '' && (
+        <div id="content">
+          <button onClick={login}>Login</button>
+        </div>
+      )}
+    </div>
   );
 };
